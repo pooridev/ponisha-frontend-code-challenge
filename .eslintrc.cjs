9019b3e0a1137c14fd44d7ae5d0c3839467066d6
Pooria Faramarzian <pooria.faramarzian@zoodfood.com>
2024-04-22 20:15:04 +0330

    feat: install & configure eslint & prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,28 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
   },
-};
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'no-var': ['error'],
+    'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
