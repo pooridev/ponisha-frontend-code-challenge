e8372688f256e3f793d76ae399a20e280f8953d3
Pooria Faramarzian <pooria.faramarzian@zoodfood.com>
2024-04-23 17:20:31 +0330

    chore: some improvements in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(async () => {
+  return {
+    plugins: [react()],
+    build: {
+      target: 'es2015',
+      outDir: 'build',
+      minify: 'terser',
+      cssMinify: true,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          entryFileNames: '[name]-[hash].js',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
     },
-    host: true,
-    strictPort: true,
-    port: 3000,
-  },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 3000,
+    },
+  }
 })
