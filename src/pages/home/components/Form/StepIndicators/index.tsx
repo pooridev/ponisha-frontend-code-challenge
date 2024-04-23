4f8c31a150bea5f64201f1232b9b95240a992716
Pooria Faramarzian <pooria.faramarzian@zoodfood.com>
2024-04-23 21:50:17 +0330

    feat(Form): add form step indicators

--- src/pages/home/components/Form/StepIndicators/index.tsx
+++ src/pages/home/components/Form/StepIndicators/index.tsx
@@ -1,12 +1,36 @@
+import { StepsListItem, formStepsList } from '../../../constants'
 import { FormStep } from '../../../hooks/useFormStep/types'
 import styles from './styles.module.css'
+import classnames from 'classnames'
 
 interface Props {
   activeStep: FormStep
 }
 
-const StepIndicators = ({ activeStepIndex, steps }: Props) => {
-  return <aside></aside>
+const StepIndicators = ({ activeStep }: Props) => {
+  const isStepActive = (step: StepsListItem) => step.id == activeStep
+
+  return (
+    <nav className={styles.stepIndicators}>
+      <ul>
+        {formStepsList.map((step, index) => (
+          <li key={step.id} className={styles.indicator}>
+            <span
+              className={classnames(styles.stepNumber, {
+                [styles.activeStepNumber]: isStepActive(step),
+              })}
+            >
+              {index + 1}
+            </span>
+            <div className={styles.stepInfo}>
+              <p>step {index + 1}</p>
+              <p>{step.title}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default StepIndicators
