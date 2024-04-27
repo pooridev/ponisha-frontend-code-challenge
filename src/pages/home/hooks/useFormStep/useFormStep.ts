868158224090bd8f0362a347273a41f45eb0f8f1
Pooria Faramarzian <pooria.faramarzian@zoodfood.com>
2024-04-27 15:52:25 +0330

    fix: reset default form step

--- src/pages/home/hooks/useFormStep/useFormStep.ts
+++ src/pages/home/hooks/useFormStep/useFormStep.ts
@@ -3,7 +3,7 @@ import { FORM_STEPS } from './constants'
 import { FormStep } from './types'
 
 export const useFormStep = () => {
-  const [activeStep, setActiveStep] = useState<FormStep>(FORM_STEPS.SELECT_PLAN)
+  const [activeStep, setActiveStep] = useState<FormStep>(FORM_STEPS.PERSONAL_INFO)
 
   const handleChangeStep = (step: FormStep) => {
     setActiveStep(step)
